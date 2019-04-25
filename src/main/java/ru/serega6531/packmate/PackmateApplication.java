f3e69c2d2ced1dd87af66878f6f9f9adce6c205d
serega6531 <shk@nextbi.ru>
2019-04-26 02:21:44 +0300

    Initial commit

---
+++ src/main/java/ru/serega6531/packmate/PackmateApplication.java
@@ -0,0 +1,13 @@
+package ru.serega6531.packmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackmateApplication.class, args);
+    }
+
+}
