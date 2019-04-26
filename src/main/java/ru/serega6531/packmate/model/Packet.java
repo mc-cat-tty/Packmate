e2164c59be7cc35bfb75a65b9d90c995572d499b
serega6531 <shk@nextbi.ru>
2019-04-26 03:35:17 +0300

    Добавлена модель стрима

--- src/main/java/ru/serega6531/packmate/model/Packet.java
+++ src/main/java/ru/serega6531/packmate/model/Packet.java
@@ -4,26 +4,26 @@ import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Data
 @Entity
 @GenericGenerator(
-        name = "stream_generator",
+        name = "packet_generator",
         strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
         parameters = {
-                @org.hibernate.annotations.Parameter(name = "sequence_name", value = "stream_seq"),
+                @org.hibernate.annotations.Parameter(name = "sequence_name", value = "packet_seq"),
                 @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
                 @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
         }
 )
-public class Stream {
+public class Packet {
 
     @Id
-    @GeneratedValue(generator = "stream_generator")
+    @GeneratedValue(generator = "packet_generator")
     private Long id;
 
-    @OneToMany(mappedBy = "stream", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Packet> packet;
+    @ManyToOne
+    @JoinColumn(name = "stream_id", nullable = false)
+    private Stream stream;
 
 }
