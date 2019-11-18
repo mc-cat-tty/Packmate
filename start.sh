f673cb0fcbc4ea69f7695ff98767386577d7b60a
saber-nyan <saber-nyan@ya.ru>
2019-11-18 17:49:42 +0300

    Added frontend as submodule, tuned networks

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-IFACE=$(ls /sys/class/net | grep '^e')
-echo "Capturing on interface ${IFACE}"
-exec java -jar app.jar --spring.datasource.url="jdbc:postgresql://packmate-db/${DB_NAME}" --spring.datasource.username="${DB_USER}" --spring.datasource.password="${DB_PASSWORD}" --interface-name="${IFACE}" --local-ip="${LOCAL_IP}" --account-login="${WEB_LOGIN}" --account-password="${WEB_PASSWORD}" --server.port=65000
+IFACE="$(ls /sys/class/net | grep '^e')"
+echo "Capturing on interface ${IFACE} with IP ${LOCAL_IP}..."
+exec java -jar app.jar --spring.datasource.url="jdbc:postgresql://127.0.0.1:65001/${DB_NAME}" --spring.datasource.username="${DB_USER}" --spring.datasource.password="${DB_PASSWORD}" --interface-name="${IFACE}" --local-ip="${LOCAL_IP}" --account-login="${WEB_LOGIN}" --account-password="${WEB_PASSWORD}" --server.port=65000
