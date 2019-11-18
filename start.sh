8f241c5ad8947f2320f9ab66147c53f79de381ac
saber-nyan <saber-nyan@ya.ru>
2019-11-18 18:20:30 +0300

    Fixed environmental variables

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 IFACE="$(ls /sys/class/net | grep '^e')"
 echo "Capturing on interface ${IFACE} with IP ${LOCAL_IP}..."
+echo "--> DEBUG: Web login is ${WEB_LOGIN}:${WEB_PASSWORD}"
 exec java -jar app.jar --spring.datasource.url="jdbc:postgresql://127.0.0.1:65001/${DB_NAME}" --spring.datasource.username="${DB_USER}" --spring.datasource.password="${DB_PASSWORD}" --interface-name="${IFACE}" --local-ip="${LOCAL_IP}" --account-login="${WEB_LOGIN}" --account-password="${WEB_PASSWORD}" --server.port=65000
