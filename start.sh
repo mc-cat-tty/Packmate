389567b46916c04fec33d2d63fd1ff72f2adc7b9
root <gambler31337@gmail.com>
2019-11-23 16:40:49 +0500

    Заставил работать через IPv4 принудительно

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ die() { echo "$*" 1>&2 ; exit 0; }  # Return 0 to stop docker container
 
 echo "Capturing on interface ${IFACE} with IP ${LOCAL_IP}..."
 echo "--> DEBUG: Web login is ${WEB_LOGIN}:${WEB_PASSWORD}"
-exec java -jar app.jar --spring.datasource.url="jdbc:postgresql://127.0.0.1:65001/${DB_NAME}" --spring.datasource.username="${DB_USER}" --spring.datasource.password="${DB_PASSWORD}" --interface-name="${IFACE}" --local-ip="${LOCAL_IP}" --account-login="${WEB_LOGIN}" --account-password="${WEB_PASSWORD}" --server.port=65000
+exec java -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses=true -jar app.jar --spring.datasource.url="jdbc:postgresql://127.0.0.1:65001/${DB_NAME}" --spring.datasource.username="${DB_USER}" --spring.datasource.password="${DB_PASSWORD}" --interface-name="${IFACE}" --local-ip="${LOCAL_IP}" --account-login="${WEB_LOGIN}" --account-password="${WEB_PASSWORD}" --server.port=65000 --server.address='0.0.0.0'
